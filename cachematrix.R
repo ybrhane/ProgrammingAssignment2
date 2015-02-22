3481833a65c44053a61225864bfc446ceb3ae5d9
Yonathan Brhane <yonathan.brhane@gmail.com>
2015-02-21 21:07:09 -0500

    Commiting cachematrix.R to local cloned copy of the ProgrammingAssignment2 Repo.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below caches the inverse of matrix in a "special" matrix object
+## so that recomputation of the inverse is avoided if it was already computed and
+## the content of the matrix have not changed.
 
-## Write a short comment describing this function
+## Define a function that create the "special matrix" object as a list of functions
+## used to set/get the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y){
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) inv <<- inverse
+	getinverse <- function() inv
+	list(set =set, get = get, 
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
+## Define a function that either returns the inverse of the matrix if it was already
+## computed/cached or solves and caches its inverse if it wasn't already computed. 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setinverse(inv)
+	inv
 }
+
+# Testing 
+
+x = matrix(c(1,2,2,3),nrow=2,ncol=2);
+class(x)
+x
+#     [,1] [,2]
+#[1,]    1    2
+#[2,]    2    3
+
+x_list = makeCacheMatrix(x);
+x_list 
+cacheSolve(x_list);
+#     [,1] [,2]
+#[1,]   -3    2
+#[2,]    2   -1
+
